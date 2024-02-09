8c26d16a2a6a6367adb70e8454fc92998a06dac6
Malcolm Rigg <malcolmrigg@gmail.com>
2024-02-09 11:04:25 +0000

    hard code login name

--- joust.sh
+++ joust.sh
@@ -7,7 +7,7 @@ fi
 
 #sets the username of the system, supervisord does not have a login
 #so we need to use the who command. 
-HOMENAME=`who | head -n1 | cut -d " " -f1`
+HOMENAME="malcolm"
 
 export HOME="/root"
 export PYTHONPATH="/home/$HOMENAME/psmoveapi/build/"
